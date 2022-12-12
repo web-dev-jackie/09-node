609d73d978490908c4cad4cbcc6ac3e07c292b87
Jackie <heatleyj7@icloud.com>
2022-12-12 11:08:58 -0500

    Working on cli.

--- 02-Challenge/Develop/index.js
+++ 02-Challenge/Develop/index.js
@@ -1,13 +1,40 @@
 // TODO: Include packages needed for this application
+const generateMarkdown = require("./utils/generateMarkdown")
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+    {
+      name: 'faveReptile',
+      message: 'What is your favorite reptile?'
+    },
+  ])
+  .then(answers => {
+    console.info('Answer:', answers.faveReptile);
+  });
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
+// link: https://www.geeksforgeeks.org/node-js-fs-writefile-method/#:~:text=writeFile()%20method%20is%20used,the%20functionality%20of%20the%20method.
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
+// 
 
 // Function call to initialize app
 init();
+
+// Extra
+// dependencies: 
+// chalk for colorful logs
+// meow for parsing
+// welcome
+
+// uses: 
+// copying ts config from a previous project
+// "npx tsconfig.json" it will ask what kind of project then assign a tsconfig from the right project.
+// then publish to npm as package
+
+// 1. build out cli
+// 2. 
--- 02-Challenge/Develop/utils/generateMarkdown.js
+++ 02-Challenge/Develop/utils/generateMarkdown.js
@@ -13,8 +13,13 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  
+  ## Description
+  ${data.description}
 
-`;
+  ## Table of Contents
+
+  `;
 }
 
 module.exports = generateMarkdown;
